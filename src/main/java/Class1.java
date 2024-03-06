e27b31bd64cde2d721e82e572be71f1309fe793f
Zuura Zheenbekova <zuurazheenbekova@Zuuras-MacBook-Pro.local>
2024-03-06 12:15:20 -0600

    updated one file and add another

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -2,5 +2,6 @@ public class Class1 {
     public static void main(String[] args) {
         ///first note
         System.out.println("First git note");
+        System.out.println("Second note");
     }
 }
